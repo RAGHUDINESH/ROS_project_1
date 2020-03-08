a695144189b96d80debfe44ad998cf114ccc6b5f
Raghu <raghudinesh98@gmail.com>
2020-03-08 12:08:21 +0530

    First ROS project

--- catkin_ws/build/catkin_generated/order_packages.py
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/raghu/ROS_project_1/catkin_ws/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/raghu/catkin_ws/devel;/opt/ros/melodic".split(';') if "/home/raghu/catkin_ws/devel;/opt/ros/melodic" != "" else []
+underlay_workspaces = "/home/raghu/ROS_project_1/catkin_ws/devel;/opt/ros/melodic".split(';') if "/home/raghu/ROS_project_1/catkin_ws/devel;/opt/ros/melodic" != "" else []
--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,9 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/raghu/ROS_project_1/catkin_ws/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/raghu/ROS_project_1/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/raghu/ROS_project_1/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/raghu/ROS_project_1/catkin_ws/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/raghu/ROS_project_1/catkin_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
-export ROS_PACKAGE_PATH="/home/raghu/ROS_project_1/catkin_ws/src:$ROS_PACKAGE_PATH"
